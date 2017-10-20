6e2e7638958ce31ff7f98b42798a0dc3ebccb011
seanzx <31815757+seanzx@users.noreply.github.com>
2017-10-19 23:34:01 -0700

    lc

---
+++ ReverseBits.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class ReverseBits {
+	public static int reverseBits(int n) {
+		if (n == 0) return 0;
+	    
+	    int result = 0;
+	    for (int i = 0; i < 32; i++) {
+	        result <<= 1;
+	        if ((n & 1) == 1) result++;
+	        n >>= 1;
+	    }
+	    return result;
+    }
+	
+	public static void main(String[] args) {
+		System.out.println(reverseBits(1));
+	}
+}
