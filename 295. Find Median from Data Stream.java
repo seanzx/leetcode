1524ae8a81c58d27a2679f6f17ef25828f322bd8
ZHIXUAN YANG <zxsean1995@gmail.com>
2017-12-17 23:16:13 -0800

    Add files via upload

---
+++ 295. Find Median from Data Stream.java
@@ -0,0 +1,24 @@
+public class MedianFinder {
+    private Queue<Long> small = new PriorityQueue(),
+                        large = new PriorityQueue();
+
+    public void addNum(int num) {
+        large.add((long) num);
+        small.add(-large.poll());
+        if (large.size() < small.size())
+            large.add(-small.poll());
+    }
+
+    public double findMedian() {
+        return large.size() > small.size()
+               ? large.peek()
+               : (large.peek() - small.peek()) / 2.0;
+    }
+};
+小堆里面最小的，和大堆里面最大的
+// Your MedianFinder object will be instantiated and called as such:
+// MedianFinder mf = new MedianFinder();
+// mf.addNum(1);
+// mf.findMedian();
+window size的，window移动的时候，如何从heap中删除元素。
+后经小哥提醒要用QuickSelect
